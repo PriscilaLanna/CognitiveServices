c3f203ea53fc8c71c1d76c9df46b3aa884db0d21
Logon Aluno <logonrmlocal@fiap.com.br>
2018-12-05 20:35:28 -0200

    Add project files.

---
+++ Bing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Search;
+using Microsoft.Azure.CognitiveServices.Search.WebSearch;
+using Microsoft.Azure.CognitiveServices.Search.WebSearch.Models;
+using Newtonsoft.Json;
+
+namespace Bing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subcriptionKey = "5783dbc5562e4297a79a3e9f2f0aca2c";
+
+            Console.WriteLine("Digite o nome de alguma personalidade");
+           var nome = Console.ReadLine();
+
+            WebSearchClient client = new WebSearchClient(new ApiKeyServiceClientCredentials(subcriptionKey));
+
+            var result = client.Web.SearchAsync(nome).Result;
+
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CognitiveServices/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Newtonsoft.Json;
+
+namespace CognitiveServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriptionKey = "002d9cf2be714679abeedb67f0bf137a";
+            var imageUrl = "https://i.pinimg.com/originals/26/c4/53/26c453cfa34d3d293398de7687ddfc8c.jpg";
+
+            ComputerVisionClient client = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey));
+
+            client.Endpoint ="https://brazilsouth.api.cognitive.microsoft.com/";
+
+            var features = new List<VisualFeatureTypes> {
+                VisualFeatureTypes.Faces,
+                VisualFeatureTypes.Tags,
+                VisualFeatureTypes.Categories,
+                VisualFeatureTypes.Description,
+                VisualFeatureTypes.Color
+      
+            };
+
+            var result = client.AnalyzeImageAsync(imageUrl).Result;
+
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+            Console.ReadLine();
+        }
+    }
+}
